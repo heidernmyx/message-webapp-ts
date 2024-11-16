f95e0c5feb1ab5c443b71363531e5438420a245d
heidernmyx <hmontejo123@gmail.com>
2024-11-16 14:44:27 +0800

    new client

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -5,3 +5,10 @@ export const createClient = () =>
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
+
+export const createAdminClient = () => {
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY!,
+  )
+}
